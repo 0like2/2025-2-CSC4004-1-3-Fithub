e7e7efb03a76d5b25a86aab66abee601fe716178
NAYEONG YOON <ynyoung227@gmail.com>
2025-11-20 18:35:27 +0900

    Delete src directory

--- src/github/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { repoService } from "./repoService";
-export { commitService } from "./commitService";
-export { treeService } from "./treeService";
-export { issueService } from "./issueService";
-export { pullService } from "./pullService";
-export { fileService } from "./fileService";
